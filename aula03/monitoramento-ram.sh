22d15e90818170f5a7c327b2227da859d2a8a75d
alego <alego@localhost.localdomain>
2019-09-06 17:32:05 -0300

    final aula 3

--- aula03/monitoramento-ram.sh
+++ aula03/monitoramento-ram.sh
@@ -1,2 +1,10 @@
 #!/bin/bash 
+mem_total=$(free | grep -i mem | awk '{ print $2 }')
+mem_usada=$(free | grep -i mem | awk '{ print $3 }')
+percent_usada=$(bc <<< "scale=2;$mem_usada/$mem_total *100" | awk -F. '{ print $1}')
 
+if [ $percent_usada -gt 15 ]
+then
+	telegram-notify --error --title “Error” --text 'Consumo de memória alto, atualmente em '$(free -h | grep -i mem | awk '{ print 2}'')'
+
+fi
